d8766794c274dc3764472f44cad0a70275b4847b
Pavellko <pavelkollegov@mail.ru>
2021-10-10 16:36:36 +0300

    v4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ fname = 'Я весь внутри плачу.fb2'
 
 with open(fname, 'rb') as f:
     data = f.read()
-    
+ 
 tree = et.fromstring(data)
 
 ns = {'ns': "http://www.gribuser.ru/xml/fictionbook/2.0"}
@@ -14,6 +14,8 @@ for bin_eb in tree.xpath('//ns:binary', namespaces=ns):
     bin_eb.getparent().remove(bin_eb)
 for bin_ed in tree.xpath('//ns:description', namespaces=ns):
     bin_ed.getparent().remove(bin_ed)
+    
+    
 cleart = et.tounicode(tree)
 cleart = re.sub(r'\<[^>]*\>', '', cleart)
 
