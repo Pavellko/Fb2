d16bd9be16d926ae3395bde04aaa48ef251a19e8
Pavellko <pavelkollegov@mail.ru>
2021-10-10 16:07:44 +0300

    v2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import lxml.etree as et
 import re
+import pyttsx3
+
 
 fname = 'Я весь внутри плачу.fb2'
 
@@ -9,13 +11,23 @@ with open(fname, 'rb') as f:
 tree = et.fromstring(data)
 
 ns = {'ns': "http://www.gribuser.ru/xml/fictionbook/2.0"}
-
 for bin_eb in tree.xpath('//ns:binary', namespaces=ns):
     bin_eb.getparent().remove(bin_eb)
 for bin_ed in tree.xpath('//ns:description', namespaces=ns):
     bin_ed.getparent().remove(bin_ed)
-global cleart, cleart2
 cleart = et.tounicode(tree)
 cleart = re.sub(r'\<[^>]*\>', '', cleart)
 
-print(cleart)
+
+engine = pyttsx3.init()
+voice = engine.getProperty('voices')
+zx={}
+for i in voice:
+    zx[i.name] = i.id
+    
+print(zx.items())
+
+engine.setProperty('voice', zx['Microsoft Irina Desktop - Russian'])
+
+engine.say(cleart)
+engine.runAndWait()
